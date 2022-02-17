14b80f7b2be8181c6b58b68c8f44b3f161cd0f42
Kacper Król <kacperkrol@protonmail.com>
2022-02-17 19:13:56 +0100

    Dopracowane templatki

--- blogposts/urls.py
+++ blogposts/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from .views import (
-    blogpost_create_view,
-    blogpost_detail_view,
-    blogpost_list_view
-)
+from .views import *
 
 app_name = 'blogposts'
 
 urlpatterns = [
     path('', blogpost_list_view, name='blogpost-list'),
     path('create/', blogpost_create_view, name='blogpost-create'),
-    path('<int:id>/', blogpost_detail_view, name='blogpost-detail')
+    path('<int:id>/', blogpost_detail_view, name='blogpost-detail'),
+    path('<int:id>/delete/', blogpost_delete_view, name='blogpost-delete'),
 ]
